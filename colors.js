2f2787888f1e46435535bec78b9b827c841a9f70
madeleinehll <hill.madeleine13@yahoo.de>
2023-05-22 16:06:22 +0200

    Farben geändert

--- colors.js
+++ colors.js
@@ -90,35 +90,35 @@ const COLORS = {
         {
             min:1,
             max:10,
-            color:"#ffff64"
+            color:"#ffffb2"
         },{
             min:10,
             max:25,
-            color:"#c8ff64"
+            color:"#b0ffbc"
         },{
             min:25,
             max:50,
-            color:"#96ff96"
+            color:"#8cffff"
         },{
             min:50,
             max:100,
-            color:"#32c8ff"
+            color:"#19cdff"
         },{
             min:100,
             max:200,
-            color:"#6496ff"
+            color:"#1982ff"
         },{
             min:200,
             max:300,
-            color:"#9664ff"
+            color:"#0f5abe"
         },{
             min:300,
             max:400,
-            color:"#ff3232"
+            color:"#784bff"
         },{
             min:400,
             max:800,
-            color:"#ff3232"
+            color:"#cd0feb"
         }
     ],
 }
